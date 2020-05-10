37b5d6979f26982f7cd9bed606c34fea046acf39
nathan.latino <nathan.latino@he-arc.ch>
2020-05-11 00:36:21 +0200

    change

--- strategy.py
+++ strategy.py
@@ -1,14 +1,20 @@
 from abc import ABC, abstractmethod
 
+import numpy as np
+
+from point import Point
+
+
 class Strategy(ABC):
 	@abstractmethod
-	def do_algorithme(selfself, data: list):
+	def do_algorithme(self, data: list):
 		pass
 
 class means_lines(Strategy):
-	def do_algorithme(selfself, data: list):
+	def do_algorithme(self, lane_data: list):
 		pass
 
+
 class means_points(Strategy):
-	def do_algorithme(selfself, data: list):
+	def do_algorithme(self, lane_data: list):
 		pass
